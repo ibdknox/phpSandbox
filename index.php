54f0dcd143fbe7da503751513367c8383e843137
MarielBellmaker <fuzzeebubbles@msn.com>
2010-05-20 21:18:21 -0500

    Test of html examples
    Signed-off-by: MarielBellmaker <fuzzeebubbles@msn.com>

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<html>
+<body style="background-color:LightBlue">
+
+<h1 style="font-family:Verdana;color:blue">
+Test page
+</h1>
+
+<a style="font-family:Times;color:yellow" href="http://www.facebook.com">
+Facebook 
+</a><br/>
+
+
+<p style="font-family:Times;color:yellow">
+And so it begins.
+</p>
+
+<img src="http://www.plushpaws.co.uk/user/products/large/CP-DUC-1w.jpg"
+title="duckster" height/>
+<br/>
+<br/>
+
+<form>
+<p style="color:yellow">
+Instrument 
+</p>
+<input type=text name="instrument">
+<input type=submit value="search">
+</form>
+
 <?php echo "hello world"; ?>
-<?php echo "hello from chris"; ?>
-<?php echo "herro fuzzy"; ?>
+
+</body>
+<html>
