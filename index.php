992bc8cb0eb2ed0946a95f647be56fb778c2c0ed
Chris Granger <chris@wrenchlabs.com>
2010-05-20 19:32:33 -0700

    reformatted and cleaned up the html a bit to be more XHTML compliant
    Signed-off-by: Chris Granger <chris@wrenchlabs.com>

--- index.php
+++ index.php
@@ -1,33 +1,25 @@
 <html>
-<body style="background-color:LightBlue">
+<head>
+	<title>ducky's page</title>
+	<style type="text/css">
+		body { background: LightBlue; font-family:Times; color:yellow}
+		a { color: yellow; }
+		h1 { font-family:Verdana; color:blue; }
+		form { margin-top:2em; }
+		p.world { color: black; }
+	</style>
+</head>
+<body>
+	<h1>Test page</h1>
+	<a href="http://www.facebook.com" target="_blank">Facebook</a>
+	<p>And so it begins.</p>
+	<img src="http://www.plushpaws.co.uk/user/products/large/CP-DUC-1w.jpg" title="duckster" height="522" width="600"/>
+	<form method="GET" action="">
+		<p>Instrument</p>
+		<input type="text" name="instrument">
+		<input type="submit" value="search">
+	</form>
 
-<h1 style="font-family:Verdana;color:blue">
-Test page
-</h1>
-
-<a style="font-family:Times;color:yellow" href="http://www.facebook.com">
-Facebook 
-</a><br/>
-
-
-<p style="font-family:Times;color:yellow">
-And so it begins.
-</p>
-
-<img src="http://www.plushpaws.co.uk/user/products/large/CP-DUC-1w.jpg"
-title="duckster" height/>
-<br/>
-<br/>
-
-<form>
-<p style="color:yellow">
-Instrument 
-</p>
-<input type=text name="instrument">
-<input type=submit value="search">
-</form>
-
-<?php echo "hello world"; ?>
-
+	<p class="world"><?php echo "hello world"; ?></p>
 </body>
 <html>
